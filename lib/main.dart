f72e9b196d77624b05f3286343b672937af64aa1
Walter Andrade <walteraandrade@gmail.com>
2021-11-28 09:46:51 -0300

    bottom tabs uncompleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:spotify_clone/src/modules/greetings.dart';
 import 'package:spotify_clone/src/atomic/vseparator.dart';
@@ -20,25 +21,9 @@ class MyApp extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final Player player = ref.watch(playerProvier);
-    return MaterialApp(
-        home: Scaffold(
-            body: Stack(
-      children: [
-        Container(
-            padding: const EdgeInsets.all(20),
-            decoration: const BoxDecoration(
-                gradient: RadialGradient(
-                    center: Alignment(-1, -1),
-                    colors: [Colors.grey, Colors.black])),
-            child: const HomeContent()),
-        Positioned(
-          child: player,
-          bottom: 20,
-          left: 10,
-          right: 10,
-        )
-      ],
-    )));
+    return const MaterialApp(
+      home: HomeLayout(),
+    );
   }
 }
 
@@ -61,6 +46,45 @@ class HomeContent extends StatelessWidget {
           VSeparator(size: 40),
         ],
       ),
+    );
+  }
+}
+
+class HomeLayout extends StatefulWidget {
+  const HomeLayout({Key? key}) : super(key: key);
+
+  @override
+  _HomeLayoutState createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                  gradient: RadialGradient(
+                      center: Alignment(-1, -1),
+                      colors: [Colors.grey, Colors.black])),
+              child: const HomeContent()),
+          Positioned(
+            child: player,
+            bottom: 20,
+            left: 10,
+            right: 10,
+          )
+        ],
+      ),
+      // ignore: prefer_const_literals_to_create_immutables
+      bottomNavigationBar: BottomNavigationBar(items: [
+        const BottomNavigationBarItem(icon: Icons.home, label: 'Home'),
+        const BottomNavigationBarItem(icon: Icons.search, label: 'Search'),
+        const BottomNavigationBarItem(
+            icon: Icons.local_library, label: 'Your library'),
+      ], currentIndex: 0, selectedItemColor: Colors.amber[700]),
     );
   }
 }
