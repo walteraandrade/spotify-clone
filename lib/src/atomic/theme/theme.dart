7abdab5c2dfbce00ef485c7579c93c581afcba55
Walter Andrade <walteraandrade@gmail.com>
2021-11-28 10:39:24 -0300

    added theme, lacks styling it

---
+++ lib/src/atomic/theme/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DefaultTheme {
+  static ThemeData get defaultTheme {
+    return ThemeData(
+        primaryColor: Colors.greenAccent[400],
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'Montserrat');
+  }
+}
