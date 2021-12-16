2e5af516bb3eca00c436f578df8472933c876d8e
Walter Andrade <walteraandrade@gmail.com>
2021-12-16 17:09:33 -0300

    removed comments

--- lib/src/modules/login-form/login_form.dart
+++ lib/src/modules/login-form/login_form.dart
@@ -29,10 +29,7 @@ class _LoginFormState extends State<LoginForm> {
               TextButton(
                 child: const Text('Submit'),
                 onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
                   if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Success')),
                     );
